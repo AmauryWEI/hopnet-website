ded9d102532050deb733f8c96725872a938d7a08
Amaury Wei <amaury.wei@gmail.com>
2025-04-25 14:00:12 +0200

    html: add interactive counterfactual videos

--- static/js/index.js
+++ static/js/index.js
@@ -1,19 +1,25 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
 
-$(document).ready(function() {
-    // Check for click events on the navbar burger icon
+$(document).ready(function () {
+	// Initialize all div with carousel class
+	var options = {
+		slidesToScroll: 1,
+		slidesToShow: 1,
+		loop: true,
+		infinite: true,
+		autoplay: false,
+		autoplaySpeed: 5000,
+	}
+	var carousels = bulmaCarousel.attach('.carousel', options);
 
-    var options = {
-			slidesToScroll: 1,
-			slidesToShow: 1,
-			loop: true,
-			infinite: true,
-			autoplay: false,
-			autoplaySpeed: 5000,
-    }
-
-		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
-
+	// Display the right counterfactual video based on the radio button
+	$('input[type="radio"').click(
+		function () {
+			const inputValue = $(this).attr("value");
+			const targetVideo = $(".obj" + inputValue);
+			$(".selectt").not(targetVideo).hide();
+			$(targetVideo).show()
+		}
+	)
 })
